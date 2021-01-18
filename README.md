b720e2e191107ad086105abace7cad5f2f48799c
Diego Vieira <diego@protos.inf.br>
2021-01-18 17:11:48 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,74 @@
+# Attention
+
+Use ate your own risk. I'm not responsible for any damage this may cause to your files. Make sure to have a backup.
+
+# KeyHelp-fixperms
+A based on [cPanel-fixperms](https://github.com/PeachFlame/cPanel-fixperms) script to fix permissions and ownership, on files and directories, for KeyHelp accounts.
+
+## More Info
+Ever needed just to quickly 'fix' the permissions or ownership for your files in a regular KeyHelp account? This is the script for you. There is a staggering number of people using KeyHelp out there, and this script will help every KeyHelp user quickly recover from self-made permission mistakes or allow you to be lazy when setting permissions when uploading new scripts (ex: Wordpress).
+
+## Instructions
+
+### Fixperms - for one single user
+
+To get the `fixperms` script, simply wget the file from GitHub and make sure it's executable:
+
+```bash
+wget https://raw.githubusercontent.com/diego-vieira/KeyHelp-fixperms/master/fixperms.sh
+chmod +x fixperms.sh
+```
+
+Then, run it (with ROOT permissions) while using the 'a' flag to specify a particular KeyHelp user:
+```bash
+sudo sh ./fixperms.sh -a USER-NAME
+```
+It does not matter which directory you are in when you run fixperms. You can be in the user’s home directory, the server root, etc... The script will not affect anything outside of the particular user’s home folder.
+
+### Fixperms - for all of the users
+If you would like fix the permissions for every user on your KeyHelp server, simply use the '-all' option:
+
+```bash
+sudo sh ./fixperms.sh -all
+```
+
+### Verbosity of Fixperms
+By default, the script runs in a 'quiet' mode with minimal display. However, if you’re like me, you may want to see everything that is happening. You can turn on verbosity and have the script print to the screen everything that is being changed. I find this extremely useful when fixing large accounts that have many files. You can watch the changes as a sort of 'progress bar' of completion. The '-v' option can be used per account or with all accounts.
+
+#### For one single account ####
+```bash
+sudo sh ./fixperms.sh -v -a USER-NAME
+```
+
+#### For all accounts ####
+```bash
+sudo sh ./fixperms.sh -v -all
+```
+
+### Getting Help
+You can run `fixperms` with the '-h' or '--help' flags in order to see a help menu.
+
+You can also open an issue here on GitHub if you see any problems.
+
+### Adding Fixperms to your bin
+I host numerous websites for friends and family, who will routinely make mistakes in regards to file permissions. It's understandable; they're not tech people. I will need to fix their permissions for them pretty frequently on my servers so I opted to put the `fixperms` script in all my servers' bin folders.
+
+```bash
+sudo mv fixperms.sh /usr/bin/fixperms
+```
+
+## History
+
+All credits for the original cPanel script goes to 
+
+https://github.com/PeachFlame/cPanel-fixperms
+
+Now that `fixperms` is in Github, all contributors will have proper credit. However, before the move to Github, there were a 2 inidividuals that were crucial to the scripts existence:
+
+- Dean Freeman
+- Colin R.
+
+
+## Contributing
+
+If you would like to contribute, simply create a new feature branch, named for the fix, and submit a merge request.
---
+++ fixperms.sh
@@ -0,0 +1,145 @@
+#! /bin/bash
+# 
+# License: GNU General Public License v3.0
+# Copyright https://github.com/PeachFlame/cPanel-fixperms
+#
+# See the Github page for full license and notes:
+# https://github.com/diego-vieira/KeyHelp-fixperms
+#
+
+# Set verbose to null
+verbose=""
+
+
+#Print the help text
+helptext () {
+    tput bold
+    tput setaf 2
+    echo "Fix perms script help:"
+    echo "Sets file/directory permissions to match suPHP and FastCGI schemes"
+    echo "USAGE: fixperms [options] -a account_name"
+    echo "-------"
+    echo "Options:"
+    echo "-h or --help: print this screen and exit"
+    echo "-v: verbose output"
+    echo "-all: run on all KeyHelp accounts"
+    echo "--account or -a: specify a KeyHelp account"
+    tput sgr0
+    exit 0
+}
+
+# Main workhorse, fix perms per account passed to it
+fixperms () {
+
+  #Get account from what is passed to the function
+  account=$1
+
+  #Check account against KeyHelp users file
+  if ! grep $account /etc/passwd
+  then
+    tput bold
+    tput setaf 1
+    echo "Invalid KeyHelp account"
+    tput sgr0
+    exit 0
+  fi
+
+  #Make sure account isn't blank
+  if [ -z $account ]
+  then
+    tput bold
+    tput setaf 1
+    echo "Need an account name!"
+    tput sgr0
+    helptext
+  #Else, start doing work
+  else
+
+    #Get the account's homedir
+    HOMEDIR=$(egrep "^amelia:" /etc/passwd | cut -d: -f6)
+
+    tput bold
+    tput setaf 4
+    echo "Fixing perms for $account:"
+    tput setaf 3
+    echo "------------------------"
+    tput setaf 4
+    echo "Fixing website files...."
+    tput sgr0
+    
+    #Fix individual files in www
+    find ${HOMEDIR}www -type d -exec chmod $verbose 755 {} \;
+    find ${HOMEDIR}www -type f | xargs -d$'\n' -r chmod $verbose 644
+    find ${HOMEDIR}www -name '*.cgi' -o -name '*.pl' | xargs -r chmod $verbose 755
+    # Hidden files support: https://serverfault.com/a/156481
+    # fix hidden files and folders like .well-known/ with root or other user perms
+    chown $verbose -R $account:$account ${HOMEDIR}www/.*
+    chown $verbose -R $account:$account ${HOMEDIR}www
+    find ${HOMEDIR}/* -name .htaccess -exec chown $verbose $account.$account {} \;
+
+    tput bold
+    tput setaf 4
+    echo "Fixing www...."
+    tput sgr0
+    #Fix perms of www itself
+    chown $verbose $account:www-data ${HOMEDIR}www
+    chmod $verbose 750 ${HOMEDIR}www
+
+    #Finished
+    tput bold
+    tput setaf 3
+    echo "Finished!"
+    echo "------------------------"
+    printf "\n\n"
+    tput sgr0
+  fi
+
+  return 0
+}
+
+#Parses all users through KeyHelp's users file
+all () {
+    for user in $(egrep "^${account}:" /etc/passwd | cut -d: -f6)
+    do
+  fixperms $user
+    done
+}
+
+#Main function, switches options passed to it
+case "$1" in
+
+    -h) helptext
+  ;;
+    --help) helptext
+      ;;
+    -v) verbose="-v"
+
+  case "$2" in
+
+    -all) all
+           ;;
+    --account) fixperms "$3"
+         ;;
+    -a) fixperms "$3"
+        ;;
+    *) tput bold
+           tput setaf 1
+       echo "Invalid Option!"
+       helptext
+       ;;
+  esac
+  ;;
+
+    -all) all
+    ;;
+    --account) fixperms "$2"
+          ;;
+    -a) fixperms "$2"
+  ;;
+    *)
+       tput bold
+       tput setaf 1
+       echo "Invalid Option!"
+       helptext
+       ;;
+esac
